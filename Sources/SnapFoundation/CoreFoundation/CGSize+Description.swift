4dfd05c4f7f9b353f619afeedaf06e21bda17912
Simon Nickel <simonnickel@googlemail.com>
2026-03-14 13:01:54 +0100

    Add CGSize description

---
+++ Sources/SnapFoundation/CoreFoundation/CGSize+Description.swift
@@ -0,0 +1,12 @@
+//
+//  SNAP - https://github.com/simonnickel/snap
+//  Created by Simon Nickel
+//
+
+import Foundation
+
+extension CGSize {
+    public var description: String {
+        "(w: \(width), h: \(height))"
+    }
+}
