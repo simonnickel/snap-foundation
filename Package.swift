0b17c1a2b91dfd4a732f123778f63cc9aa398b8e
Simon Nickel <simonnickel@googlemail.com>
2026-04-25 21:44:49 +0200

    Update to Swift 6.3

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 6.2
+// swift-tools-version: 6.3
 
 import PackageDescription
 
@@ -21,5 +21,4 @@ let package = Package(
             dependencies: ["SnapFoundation"]
         ),
     ],
-    swiftLanguageModes: [.version("6")]
 )
